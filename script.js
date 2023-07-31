475ae77e1519a8c37cab84a619a8dd37104d3ed7
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-31 23:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 function volume_sphere() {
-  const radiusInput = document.getElementById('radius');
-  const volumeInput = document.getElementById('volume');
-
-  const radius = parseFloat(radiusInput.value);
-
-  if (!isNaN(radius)) {
-    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-    volumeInput.value = volume.toFixed(2);
-  } else {
-    volumeInput.value = '';
-  }
-
-  return false; // Prevent form submission
-}
-
-window.onload = function () {
-  document.getElementById('MyForm').onsubmit = volume_sphere;
-};
+            var radius = document.getElementById('radius').value;
+            radius = Math.abs(radius);
+            var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            volume = volume.toFixed(4);
+            document.getElementById('volume').value = volume;
+            return false;
+        }
+ 
+      window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
